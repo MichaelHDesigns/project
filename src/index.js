9617d5518ce365cf8d282ac84fa88747ccebf167
Devilking6105 <43361618+MichaelHDesigns@users.noreply.github.com>
2023-03-22 19:53:17 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,14 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import './src/css/index.css';
-import App from './src/App';
+import Login from './Login';
 import reportWebVitals from './src/reportWebVitals';
 
-import Login from "./Login";
+ReactDOM.render(<Login />, document.getElementById('root'));
 
-ReactDOM.render(<Login />, document.getElementById("root"));
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
