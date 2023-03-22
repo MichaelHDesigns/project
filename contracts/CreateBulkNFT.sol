9b290ef5813f8ddde4c589a2733bc899006332a3
Devilking6105 <43361618+MichaelHDesigns@users.noreply.github.com>
2023-03-22 19:23:01 -0400

    Update CreateBulkNFT.sol

--- contracts/CreateBulkNFT.sol
+++ contracts/CreateBulkNFT.sol
@@ -5,7 +5,7 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "@nomiclabs/buidler/console.sol";
-import Web3 from 'web3';
+
 
 // Import Pinata SDK for uploading files to IPFS
 
