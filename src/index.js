03b12dc288855504aaa5b28e57035d9c69f9fe62
Devilking6105 <43361618+MichaelHDesigns@users.noreply.github.com>
2023-03-22 19:50:54 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,15 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 import './src/css/index.css';
 import App from './src/App';
 import reportWebVitals from './src/reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import Login from "./Login";
+
+ReactDOM.render(<Login />, document.getElementById("root"));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
