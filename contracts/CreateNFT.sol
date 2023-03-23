d0d67fe9d6c8868396e5dd1a4e5c1a3584eca08a
Devilking6105 <43361618+MichaelHDesigns@users.noreply.github.com>
2023-03-23 00:17:04 -0400

    Update CreateNFT.sol

--- contracts/CreateNFT.sol
+++ contracts/CreateNFT.sol
@@ -1,30 +1,59 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract CreateNFT is ERC721URIStorage, Ownable {
+contract CreateNFT is ERC721, Ownable {
     using SafeMath for uint256;
-
-    uint256 public tokenCounter;
-
-    constructor() ERC721("NFT Marketplace", "NFTM") {}
-
-    function createToken(string memory tokenURI) public onlyOwner returns (uint256) {
-        uint256 newTokenId = tokenCounter.add(1);
-        _safeMint(msg.sender, newTokenId);
-        _setTokenURI(newTokenId, tokenURI);
-        tokenCounter = newTokenId;
-        return newTokenId;
+    
+    string private _baseURI;
+    uint256 private _tokenIdTracker;
+    uint256 private _maxTokens;
+    uint256 private _tokenPrice;
+    bool private _saleActive;
+    
+    constructor(string memory name, string memory symbol, string memory baseURI, uint256 maxTokens, uint256 tokenPrice) ERC721(name, symbol) {
+        _baseURI = baseURI;
+        _maxTokens = maxTokens;
+        _tokenPrice = tokenPrice;
     }
-
-    function setBaseURI(string memory _newBaseURI) external onlyOwner {
-        _setBaseURI(_newBaseURI);
+    
+    modifier saleIsOpen {
+        require(totalSupply() < _maxTokens, "Sale has already ended");
+        require(_saleActive == true, "Sale is not active");
+        _;
     }
-
+    
+    function mint(uint256 numberOfTokens) public payable saleIsOpen {
+        require(numberOfTokens > 0, "You need to mint at least 1 NFT");
+        require(totalSupply().add(numberOfTokens) <= _maxTokens, "Exceeds maximum NFT limit");
+        require(msg.value >= _tokenPrice.mul(numberOfTokens), "Ether value sent is not correct");
+        
+        for (uint i = 0; i < numberOfTokens; i++) {
+            _safeMint(msg.sender, _tokenIdTracker);
+            _tokenIdTracker = _tokenIdTracker.add(1);
+        }
+    }
+    
     function _baseURI() internal view virtual override returns (string memory) {
-        return "https://nft-marketplace.com/token/";
+        return _baseURI;
+    }
+    
+    function setBaseURI(string memory _newBaseURI) external onlyOwner {
+        _baseURI = _newBaseURI;
+    }
+    
+    function startSale() external onlyOwner {
+        _saleActive = true;
+    }
+    
+    function stopSale() external onlyOwner {
+        _saleActive = false;
+    }
+    
+    function withdraw() external onlyOwner {
+        payable(msg.sender).transfer(address(this).balance);
     }
 }
