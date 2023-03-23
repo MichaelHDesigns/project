4908276e83a918ea20e15bd338fde21b8bcc3d44
Devilking6105 <43361618+MichaelHDesigns@users.noreply.github.com>
2023-03-22 21:23:47 -0400

    Update CreateBulkNFT.sol

--- contracts/CreateBulkNFT.sol
+++ contracts/CreateBulkNFT.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
-import { PinataSDK } from '@pinata/sdk';
+import { PinataSDK } from "@pinata/sdk";
 
 contract CreateBulkNFT is ERC721 {
     using Counters for Counters.Counter;
