0420487b6e84a26acc56a0e7035c12b9ee2e7937
Devilking6105 <43361618+MichaelHDesigns@users.noreply.github.com>
2023-03-23 00:20:56 -0400

    Update CreateNFT.sol

--- contracts/CreateNFT.sol
+++ contracts/CreateNFT.sol
@@ -7,113 +7,29 @@ import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 contract CreateNFT is ERC721 {
     using SafeMath for uint256;
 
-    // Base URI
-    string private _baseURI;
-
-    // Max tokens
     uint256 private _maxTokens;
+    uint256 private _tokenPrice;
 
-    // Price
-    uint256 private _price;
-
-    // Sale status
-    bool private _saleActive;
-
-    // Total supply
-    uint256 private _totalSupply;
-
-    // Event for token creation
-    event NFTCreated(uint256 tokenId);
-
-    constructor(
-        string memory baseURI_,
-        uint256 maxTokens_,
-        uint256 price_
-    ) ERC721("MyNFT", "MNFT") {
-        _baseURI = baseURI_;
-        _maxTokens = maxTokens_;
-        _price = price_;
-        _saleActive = true;
+    constructor(uint256 maxTokens, uint256 tokenPrice) ERC721("MyNFT", "MNFT") {
+        _maxTokens = maxTokens;
+        _tokenPrice = tokenPrice;
     }
 
-    // Only owner
-    modifier onlyOwner() {
-        require(msg.sender == owner(), "Only the contract owner can call this function");
-        _;
-    }
-
-    // Mint a single NFT
-    function mint() public payable {
-        require(_saleActive, "Sale has ended");
-        require(totalSupply() < _maxTokens, "Exceeds maximum NFT limit");
-        require(msg.value == _price, "Incorrect Ether value");
-
-        uint256 tokenId = totalSupply().add(1);
-        _safeMint(msg.sender, tokenId);
-
-        _totalSupply = _totalSupply.add(1);
-
-        emit NFTCreated(tokenId);
-    }
-
-    // Mint multiple NFTs at once
-    function bulkMint(uint256 numberOfTokens) public payable {
-        require(_saleActive, "Sale has ended");
+    function mintNFT(uint256 numberOfTokens) external payable {
         require(totalSupply().add(numberOfTokens) <= _maxTokens, "Exceeds maximum NFT limit");
-        require(msg.value == _price.mul(numberOfTokens), "Incorrect Ether value");
+        require(msg.value >= _tokenPrice.mul(numberOfTokens), "Ether value sent is not correct");
 
         for (uint256 i = 0; i < numberOfTokens; i++) {
-            uint256 tokenId = totalSupply().add(1);
+            uint256 tokenId = totalSupply();
             _safeMint(msg.sender, tokenId);
-
-            _totalSupply = _totalSupply.add(1);
-
-            emit NFTCreated(tokenId);
         }
     }
 
-    // Set base URI
-    function setBaseURI(string memory baseURI_) external onlyOwner {
-        _baseURI = baseURI_;
-    }
-
-    // Get base URI
-    function _baseURI() internal view virtual override returns (string memory) {
-        return _baseURI;
-    }
-
-    // Pause sale
-    function pauseSale() external onlyOwner {
-        _saleActive = false;
-    }
-
-    // Resume sale
-    function resumeSale() external onlyOwner {
-        _saleActive = true;
-    }
-
-    // End sale
-    function endSale() external onlyOwner {
-        _saleActive = false;
-    }
-
-    // Get max tokens
-    function getMaxTokens() public view returns (uint256) {
-        return _maxTokens;
-    }
-
-    // Get price
-    function getPrice() public view returns (uint256) {
-        return _price;
-    }
-
-    // Get sale status
-    function getSaleStatus() public view returns (bool) {
-        return _saleActive;
-    }
-
-    // Get total supply
     function totalSupply() public view returns (uint256) {
-        return _totalSupply;
+        return ERC721.totalSupply();
+    }
+
+    function _baseURI() internal view virtual override returns (string memory) {
+        return "https://www.example.com/";
     }
 }
