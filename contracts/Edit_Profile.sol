0bc27c2379a1727b46489a29bd8f7163817da910
Devilking6105 <43361618+MichaelHDesigns@users.noreply.github.com>
2023-03-23 01:08:38 -0400

    Update Edit_Profile.sol

--- contracts/Edit_Profile.sol
+++ contracts/Edit_Profile.sol
@@ -5,25 +5,24 @@ import "./Profile.sol";
 import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/IERC721Metadata.sol";
 
-
 contract Edit_Profile {
     Profile public profileContract;
-    
+
     constructor(Profile _profileContract) {
         profileContract = _profileContract;
     }
-    
-    function updateName(string memory newName) public {
-        profileContract.updateName(newName);
+
+    function updateName(string calldata newName) public {
+        profileContract.setName(newName);
     }
-    
-    function updateBio(string memory newBio) public {
-        profileContract.updateBio(newBio);
+
+    function updateBio(string calldata newBio) public {
+        profileContract.setBio(newBio);
     }
-    
+
     function updateProfileImage(address nftContract, uint256 tokenId) public {
         require(IERC721(nftContract).ownerOf(tokenId) == msg.sender, "You don't own this NFT.");
         string memory ipfsHash = IERC721Metadata(nftContract).tokenURI(tokenId);
-        profileContract.updateProfileImage(ipfsHash);
+        profileContract.setProfileImage(ipfsHash);
     }
 }
