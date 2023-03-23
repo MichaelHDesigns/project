3661cd953c2ae325515e8682e06bc5c7f0a34b1b
Devilking6105 <43361618+MichaelHDesigns@users.noreply.github.com>
2023-03-22 20:33:02 -0400

    Update Create_Account.sol

--- contracts/Create_Account.sol
+++ contracts/Create_Account.sol
@@ -3,8 +3,8 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/security/Pausable.sol";
-import "../js/Login.jsx";
-import "./Profile.sol";
+import '../js/Login';
+import './Profile.sol';
 
 contract Create_Account is Ownable, Pausable {
     Login public login;
