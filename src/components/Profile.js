abd9b1db4b7d36fd6dac69590f5ae926cd53cba6
Devilking6105 <43361618+MichaelHDesigns@users.noreply.github.com>
2023-03-23 02:09:49 -0400

    Update Profile.js

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { ethers } from 'ethers';
 import EditProfile from './EditProfile';
+import axios from 'axios';
 
 const Profile = ({ address, setLoggedIn }) => {
   const [profileImage, setProfileImage] = useState('');
@@ -17,7 +18,7 @@ const Profile = ({ address, setLoggedIn }) => {
 
         const profile = await contract.getProfile(address);
 
-        setProfileImage(profile.profileImageTokenId);
+        setProfileImage(profile.profileImageHash);
         setName(profile.name);
         setBio(profile.bio);
       } catch (err) {
@@ -44,6 +45,34 @@ const Profile = ({ address, setLoggedIn }) => {
     }
   };
 
+  const handleProfileImageUpload = async (e) => {
+    try {
+      const file = e.target.files[0];
+      const formData = new FormData();
+      formData.append('file', file);
+
+      const response = await axios.post('https://api.pinata.cloud/pinning/pinFileToIPFS', formData, {
+        maxContentLength: 'Infinity',
+        headers: {
+          'Content-Type': `multipart/form-data; boundary=${formData._boundary}`,
+          'pinata_api_key': process.env.REACT_APP_PINATA_API_KEY,
+          'pinata_secret_api_key': process.env.REACT_APP_PINATA_SECRET_API_KEY
+        }
+      });
+
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      const signer = provider.getSigner();
+      const contract = new ethers.Contract(process.env.REACT_APP_PROFILE_ADDRESS, process.env.REACT_APP_PROFILE_ABI, signer);
+
+      await contract.setProfileImage(response.data.IpfsHash);
+
+      setProfileImage(response.data.IpfsHash);
+    } catch (err) {
+      console.error(err);
+      alert('An error occurred while uploading your profile image.');
+    }
+  };
+
   return (
     <>
       <div>
@@ -52,10 +81,13 @@ const Profile = ({ address, setLoggedIn }) => {
         <Link to="/editprofile"><button>Edit Profile</button></Link>
       </div>
       <div>
-        <img src={`https://ipfs.io/ipfs/${profileImage}`} alt="Profile" />
+        <img src={`https://gateway.pinata.cloud/ipfs/${profileImage}`} alt="Profile" />
         <h1>{name}</h1>
         <p>{bio}</p>
         <button onClick={handleBurnSAT}>Burn SAT</button>
+        <div>
+          <input type="file" onChange={handleProfileImageUpload} />
+        </div>
       </div>
       <div>
         <EditProfile setLoggedIn={setLoggedIn} />
