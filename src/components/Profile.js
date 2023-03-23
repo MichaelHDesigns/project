d2721bdf9d97bc3f2c59c9563aad0367b14e6067
Devilking6105 <43361618+MichaelHDesigns@users.noreply.github.com>
2023-03-22 20:31:08 -0400

    Update Profile.js

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
-import React from "react";
-import { Link } from "react-router-dom";
+import React from 'react';
+import { Link } from 'react-router-dom';
 import { ethers } from 'ethers';
 import ProfileContract from './contracts/Profile.sol';
 import './src/css/Profile.css';
