53880ff1d8278520817e03c505c225d46bc23717
Devilking6105 <43361618+MichaelHDesigns@users.noreply.github.com>
2023-03-23 00:54:27 -0400

    Update CreateNFTPage.js

--- src/components/CreateNFTPage.js
+++ src/components/CreateNFTPage.js
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import CreateNFT from "../contracts/CreateNFT.sol";
-import './CreateNFTPage.css';
+import "./CreateNFTPage.css";
 
 const CONTRACT_ADDRESS = process.env.REACT_APP_CONTRACT_ADDRESS;
 
