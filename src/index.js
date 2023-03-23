0243a907884a195fd596a31d49f7bb084c4a7a3b
Devilking6105 <43361618+MichaelHDesigns@users.noreply.github.com>
2023-03-22 23:32:31 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import './src/css/index.css';
-import Login from '../js/Login.jsx';
-import reportWebVitals from './src/reportWebVitals';
+import './css/index.css';
+import Login from './js/Login.jsx';
+
+import reportWebVitals from './reportWebVitals';
+
 
 ReactDOM.render(<Login />, document.getElementById('root'));
 
