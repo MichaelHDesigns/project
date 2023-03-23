40e42cc30f5b5e56277af012a2b7b2d1b2c1e58c
Devilking6105 <43361618+MichaelHDesigns@users.noreply.github.com>
2023-03-23 00:10:48 -0400

    Update CreateNFT.sol

--- contracts/CreateNFT.sol
+++ contracts/CreateNFT.sol
@@ -1,37 +1,32 @@
 // SPDX-License-Identifier: MIT
+
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract CreateNFT is ERC721 {
+contract CreateNFT is ERC721, Ownable {
 
-    string private baseURI;
-    uint256 private tokenCounter;
-    address public owner;
+    string private _baseURI;
+    uint256 private _tokenCounter;
 
-    modifier onlyOwner() {
-        require(msg.sender == owner, "Only the contract owner can call this function.");
-        _;
+    constructor(string memory baseURI) ERC721("MyNFT", "MNFT") {
+        _baseURI = baseURI;
+        _tokenCounter = 0;
     }
 
-    constructor(string memory _baseURI) ERC721("MyNFT", "MNFT") {
-        baseURI = _baseURI;
-        tokenCounter = 0;
-        owner = msg.sender;
-    }
-
-    function mintNFT(string memory _tokenURI) external {
-        uint256 newTokenId = tokenCounter;
+    function mintNFT(string memory tokenURI) external {
+        uint256 newTokenId = _tokenCounter;
         _safeMint(msg.sender, newTokenId);
-        _setTokenURI(newTokenId, _tokenURI);
-        tokenCounter++;
+        _setTokenURI(newTokenId, tokenURI);
+        _tokenCounter++;
     }
 
-    function setBaseURI(string memory _newBaseURI) external onlyOwner {
-        baseURI = _newBaseURI;
+    function setBaseURI(string memory newBaseURI) external onlyOwner {
+        _baseURI = newBaseURI;
     }
 
-    function _baseURI() internal view override returns (string memory) {
-        return baseURI;
+    function _baseURI() internal view virtual override returns (string memory) {
+        return _baseURI;
     }
 }
