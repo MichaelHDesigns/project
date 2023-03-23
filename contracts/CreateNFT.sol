972a8ee229a0c31b72694cdf105304e93e1c9f59
Devilking6105 <43361618+MichaelHDesigns@users.noreply.github.com>
2023-03-23 00:09:38 -0400

    Update CreateNFT.sol

--- contracts/CreateNFT.sol
+++ contracts/CreateNFT.sol
@@ -1,5 +1,4 @@
 // SPDX-License-Identifier: MIT
-
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
@@ -8,10 +7,17 @@ contract CreateNFT is ERC721 {
 
     string private baseURI;
     uint256 private tokenCounter;
+    address public owner;
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Only the contract owner can call this function.");
+        _;
+    }
 
     constructor(string memory _baseURI) ERC721("MyNFT", "MNFT") {
         baseURI = _baseURI;
         tokenCounter = 0;
+        owner = msg.sender;
     }
 
     function mintNFT(string memory _tokenURI) external {
@@ -27,13 +33,5 @@ contract CreateNFT is ERC721 {
 
     function _baseURI() internal view override returns (string memory) {
         return baseURI;
-    }
-
-    function tokenURI(uint256 _tokenId) public view override(ERC721) returns (string memory) {
-        return string(abi.encodePacked(baseURI, _tokenId.toString()));
-    }
-
-    function setTokenURI(uint256 _tokenId, string memory _tokenURI) external onlyOwner {
-        _setTokenURI(_tokenId, _tokenURI);
     }
 }
