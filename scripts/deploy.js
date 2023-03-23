13943ebc4f83e321ac50e9ce3d6a675964a30f34
Devilking6105 <43361618+MichaelHDesigns@users.noreply.github.com>
2023-03-22 21:21:04 -0400

    Update deploy.js

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -7,10 +7,19 @@ async function main() {
 
   const tokenFactory = await hre.ethers.getContractFactory("AuthToken");
   const token = await tokenFactory.deploy();
+  
+  const dashboardFactory = await hre.ethers.getContractFactory("Dashboard");
+  const account = await accountFactory.deploy(token.address);
 
   const accountFactory = await hre.ethers.getContractFactory("Create_Account");
   const account = await accountFactory.deploy(token.address);
+  
+  const editProfileFactory = await hre.ethers.getContractFactory("Edit_Profile");
+  const account = await accountFactory.deploy(token.address);
 
+  const profileFactory = await hre.ethers.getContractFactory("Profile");
+  const account = await accountFactory.deploy(token.address);
+  
   const nftFactory = await hre.ethers.getContractFactory("CreateNFT");
   const nft = await nftFactory.deploy(account.address);
 
@@ -29,6 +38,9 @@ async function main() {
   await bulkNft.deployed();
   await collection.deployed();
   await marketplace.deployed();
+  await dashboard.deployed();
+  await editProfile.deployed();
+  await profile.deployed();
 
   console.log("Token deployed to:", token.address);
   console.log("CreateAccount deployed to:", account.address);
@@ -36,6 +48,9 @@ async function main() {
   console.log("CreateBulkNFT deployed to:", bulkNft.address);
   console.log("CreateCollection deployed to:", collection.address);
   console.log("Marketplace deployed to:", marketplace.address);
+  console.log("Dashboard deployed to:", dashboard.address);
+  console.log("Edit_Profile deployed to:", editProfile.address);
+  console.log("{Profile deployed to:", profile.address);
 }
 
 main().then(() => process.exit(0)).catch(error => {
