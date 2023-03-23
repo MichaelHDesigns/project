c96e63c5e49522dac0e17a1062fcb68fb56da1eb
Devilking6105 <43361618+MichaelHDesigns@users.noreply.github.com>
2023-03-23 03:13:44 -0400

    Update CreateNFT.sol

--- contracts/CreateNFT.sol
+++ contracts/CreateNFT.sol
@@ -1,52 +1,28 @@
 // SPDX-License-Identifier: MIT
+
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
-import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "./Create_Account.sol";
 
-contract CreateNFT is ERC721Enumerable {
-    using SafeMath for uint256;
+contract CreateNFT is ERC721 {
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
 
-    uint256 private _maxTokens;
-    uint256 private _tokenPrice;
+    Create_Account private _createAccount;
 
-    constructor(uint256 maxTokens, uint256 tokenPrice) ERC721("MyNFT", "MNFT") {
-        _maxTokens = maxTokens;
-        _tokenPrice = tokenPrice;
+    constructor(address createAccountAddress) ERC721("MyNFT", "NFT") {
+        _createAccount = Create_Account(createAccountAddress);
     }
 
-    function mintNFT(uint256 numberOfTokens) external payable {
-        require(totalSupply().add(numberOfTokens) <= _maxTokens, "Exceeds maximum NFT limit");
-        require(msg.value >= _tokenPrice.mul(numberOfTokens), "Ether value sent is not correct");
+    function createNFT(address recipient) public returns (uint256) {
+        require(_createAccount.doesAccountExist(msg.sender), "Sender does not have a registered account");
+        _tokenIds.increment();
 
-        for (uint256 i = 0; i < numberOfTokens; i++) {
-            uint256 tokenId = totalSupply();
-            _safeMint(msg.sender, tokenId);
-        }
-    }
+        uint256 newItemId = _tokenIds.current();
+        _mint(recipient, newItemId);
 
-    function mintBulkNFT(uint256[] memory tokenIds) external {
-        for (uint256 i = 0; i < tokenIds.length; i++) {
-            require(totalSupply() < _maxTokens, "Exceeds maximum NFT limit");
-            _safeMint(msg.sender, tokenIds[i]);
-        }
-    }
-
-    function mintNFTCollection(uint256 numberOfTokens) external payable {
-        require(totalSupply().add(numberOfTokens) <= _maxTokens, "Exceeds maximum NFT limit");
-        require(msg.value >= _tokenPrice.mul(numberOfTokens), "Ether value sent is not correct");
-
-        for (uint256 i = 0; i < numberOfTokens; i++) {
-            uint256 tokenId = totalSupply();
-            _safeMint(msg.sender, tokenId);
-        }
-    }
-
-    function totalSupply() public view override returns (uint256) {
-        return ERC721Enumerable.totalSupply();
-    }
-
-    function _baseURI() internal view virtual override returns (string memory) {
-        return "https://www.example.com/";
+        return newItemId;
     }
 }
