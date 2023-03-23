2bab05fea12693b37e78c364b3c57622d983c703
Devilking6105 <43361618+MichaelHDesigns@users.noreply.github.com>
2023-03-23 00:50:44 -0400

    Update CreateNFT.sol

--- contracts/CreateNFT.sol
+++ contracts/CreateNFT.sol
@@ -1,10 +1,10 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 
-contract CreateNFT is ERC721 {
+contract CreateNFT is ERC721Enumerable {
     using SafeMath for uint256;
 
     uint256 private _maxTokens;
@@ -25,8 +25,25 @@ contract CreateNFT is ERC721 {
         }
     }
 
-    function totalSupply() public view returns (uint256) {
-        return ERC721.totalSupply();
+    function mintBulkNFT(uint256[] memory tokenIds) external {
+        for (uint256 i = 0; i < tokenIds.length; i++) {
+            require(totalSupply() < _maxTokens, "Exceeds maximum NFT limit");
+            _safeMint(msg.sender, tokenIds[i]);
+        }
+    }
+
+    function mintNFTCollection(uint256 numberOfTokens) external payable {
+        require(totalSupply().add(numberOfTokens) <= _maxTokens, "Exceeds maximum NFT limit");
+        require(msg.value >= _tokenPrice.mul(numberOfTokens), "Ether value sent is not correct");
+
+        for (uint256 i = 0; i < numberOfTokens; i++) {
+            uint256 tokenId = totalSupply();
+            _safeMint(msg.sender, tokenId);
+        }
+    }
+
+    function totalSupply() public view override returns (uint256) {
+        return ERC721Enumerable.totalSupply();
     }
 
     function _baseURI() internal view virtual override returns (string memory) {
