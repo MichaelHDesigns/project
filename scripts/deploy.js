6e47d2e9236ab2665b849a567e2e8c9cc466c3e4
Devilking6105 <43361618+MichaelHDesigns@users.noreply.github.com>
2023-03-23 02:33:04 -0400

    Update deploy.js

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -3,27 +3,52 @@ const hre = require("hardhat");
 async function main() {
   const [deployer] = await hre.ethers.getSigners();
 
-  const token = await hre.ethers.getContractFactory("AuthToken").deploy();
+  console.log("Deploying contracts with the account:", deployer.address);
 
-  const factories = [
-    { name: "Login", args: [token.address] },
-    { name: "Dashboard", args: [] },
-    { name: "Create_Account", args: [token.address] },
-    { name: "ProfileEditor", args: [token.address] },
-    { name: "Profile", args: [token.address] },
-    { name: "CreateNFT", args: [token.address] },
-    { name: "Marketplace", args: [token.address] },
-  ];
+  const tokenFactory = await hre.ethers.getContractFactory("AuthToken");
+  const token = await tokenFactory.deploy();
+  
+  
+  const loginFactory = await hre.ethers.getContractFactory("Login");
+  const login = await loginFactory.deploy(account.address);
 
-  for (const { name, args } of factories) {
-    const contract = await hre.ethers.getContractFactory(name).deploy(...args);
-    console.log(`${name} deployed to:`, contract.address);
-  }
+  const accountFactory = await hre.ethers.getContractFactory("Create_Account");
+  const account = await accountFactory.deploy(token.address);
+
+  const nftFactory = await hre.ethers.getContractFactory("CreateNFT");
+  const nft = await nftFactory.deploy(account.address);
+
+  const ProfileFactory = await hre.ethers.getContractFactory("Profile");
+  const profile = await profileFactory.deploy(account.address);
+
+  const dashboardFactory = await hre.ethers.getContractFactory("Dashboard");
+  const dashboard = await dashboardFactory.deploy(account.address);
+
+  const marketplaceFactory = await hre.ethers.getContractFactory("Marketplace");
+  const marketplace = await marketplaceFactory.deploy(account.address, token.address);
+  
+  
+  const editprofileFactory = await hre.ethers.getContractFactory("ProfileEditor");
+  const editprofile = await editprofileFactory.deploy(account.address, token.address);
+
+  await token.deployed();
+  await account.deployed();
+  await profile.deployed();
+  await nft.deployed();
+  await dashboard.deployed();
+  await marketplace.deployed();
+  await login.deployed();
+
+  console.log("Token deployed to:", token.address);
+  console.log("CreateAccount deployed to:", account.address);
+  console.log("CreateNFT deployed to:", nft.address);
+  console.log("Dashboard deployed to:", dashboard.address);
+  console.log("Login deployed to:", login.address);
+  console.log("Profile deployed to:", profile.address);
+  console.log("Marketplace deployed to:", marketplace.address);
 }
 
-main()
-  .then(() => process.exit(0))
-  .catch(error => {
-    console.error(error);
-    process.exit(1);
-  });
+main().then(() => process.exit(0)).catch(error => {
+  console.error(error);
+  process.exit(1);
+});
