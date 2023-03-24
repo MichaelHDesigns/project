a70d798c07ba23c4a8076e5235ee983d193ff0ae
Devilking6105 <43361618+MichaelHDesigns@users.noreply.github.com>
2023-03-24 04:47:25 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
+
 import React, { useState } from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import Create_Account from './components/Create_Account';
 import Dashboard from './components/Dashboard';
 import EditProfile from './components/ProfileEditor';
@@ -13,10 +14,34 @@ import './css/index.css';
 
 function App() {
   const [loggedIn, setLoggedIn] = useState(false);
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const handleSearchChange = (event) => {
+    setSearchTerm(event.target.value);
+  }
 
   return (
     <Router>
       <div className="App">
+        <nav className="navbar">
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/marketplace">Marketplace</Link>
+            </li>
+            <li>
+              <Link to="/create_nft">Create NFT</Link>
+            </li>
+            <li>
+              <form>
+                <input type="text" placeholder="Search" value={searchTerm} onChange={handleSearchChange} />
+                <button type="submit">Search</button>
+              </form>
+            </li>
+          </ul>
+        </nav>
         <Switch>
           <Route exact path="/" component={LandingPage} />
           <Route exact path="/login" render={() => <Login setLoggedIn={setLoggedIn} />} />
