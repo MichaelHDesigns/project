6815cac94614ba9cf3ee1a5c99a514043707ad28
Devilking6105 <43361618+MichaelHDesigns@users.noreply.github.com>
2023-03-24 03:25:24 -0400

    Update Dashboard.sol

--- contracts/Dashboard.sol
+++ contracts/Dashboard.sol
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: MIT
-
 pragma solidity ^0.8.0;
+
+import "./CreateNFT.sol";
 
 contract Dashboard {
     struct UserProfile {
@@ -11,8 +12,19 @@ contract Dashboard {
     }
 
     mapping(address => UserProfile) private userProfiles;
+    mapping(address => uint256[]) private userNfts;
+    mapping(address => mapping(uint256 => uint256[])) private userNftCollections;
 
     event UserProfileUpdated(address indexed userAddress, string name, string email, string bio, string profileImageUrl);
+    event UserNftMinted(address indexed userAddress, uint256 indexed tokenId);
+    event UserNftBulkMinted(address indexed userAddress, uint256[] tokenIds);
+    event UserNftCollectionCreated(address indexed userAddress, uint256 indexed collectionId, uint256[] tokenIds);
+
+    CreateNFT private _createNft;
+
+    constructor(address createNftAddress) {
+        _createNft = CreateNFT(createNftAddress);
+    }
 
     function updateUserProfile(string memory name, string memory email, string memory bio, string memory profileImageUrl) public {
         UserProfile storage userProfile = userProfiles[msg.sender];
@@ -43,4 +55,40 @@ contract Dashboard {
     function getProfileImageUrl() public view returns (string memory) {
         return userProfiles[msg.sender].profileImageUrl;
     }
-}
+
+    function mintNft() public returns (uint256) {
+        uint256 newItemId = _createNft.createNFT(msg.sender);
+        userNfts[msg.sender].push(newItemId);
+
+        emit UserNftMinted(msg.sender, newItemId);
+        return newItemId;
+    }
+
+    function bulkMintNfts(uint256 numberOfNfts) public returns (uint256[] memory) {
+        uint256[] memory tokenIds = new uint256[](numberOfNfts);
+        for (uint256 i = 0; i < numberOfNfts; i++) {
+            uint256 newItemId = _createNft.createNFT(msg.sender);
+            userNfts[msg.sender].push(newItemId);
+            tokenIds[i] = newItemId;
+        }
+
+        emit UserNftBulkMinted(msg.sender, tokenIds);
+        return tokenIds;
+    }
+
+    function createNftCollection(uint256[] memory tokenIds) public returns (uint256) {
+        require(tokenIds.length > 0, "At least one token id is required");
+        uint256 collectionId = userNftCollections[msg.sender][tokenIds[0]].length;
+        for (uint256 i = 0; i < tokenIds.length; i++) {
+            require(ownerOf(tokenIds[i]) == msg.sender, "Sender does not own all tokens");
+            userNftCollections[msg.sender][tokenIds[i]].push(collectionId);
+        }
+
+        emit UserNftCollectionCreated(msg.sender, collectionId, tokenIds);
+        return collectionId;
+    }
+
+    function getUserNfts() public view returns (uint256[] memory) {
+        return userNfts[msg.sender];
+    }
+
