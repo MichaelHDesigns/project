b74850e030fd9eaf6338ef4b5374bc4666a4ecbf
Devilking6105 <43361618+MichaelHDesigns@users.noreply.github.com>
2023-03-24 03:34:54 -0400

    Update Dashboard.js

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,3 +1,4 @@
+
 import React, { useState, useEffect } from 'react';
 import { ethers } from 'ethers';
 import Dashboard from '../contracts/Dashboard.sol';
@@ -5,11 +6,7 @@ import './Dashboard.css';
 
 function DashboardComponent(props) {
   const [dashboardContract, setDashboardContract] = useState(null);
-  const [userAddress, setUserAddress] = useState('');
-  const [userName, setUserName] = useState('');
-  const [userEmail, setUserEmail] = useState('');
-  const [userBio, setUserBio] = useState('');
-  const [profileImageUrl, setProfileImageUrl] = useState('');
+  const [userProfile, setUserProfile] = useState({});
 
   // Set up the Ethereum provider
   const provider = new ethers.providers.Web3Provider(window.ethereum);
@@ -18,7 +15,7 @@ function DashboardComponent(props) {
   useEffect(() => {
     async function setupDashboardContract() {
       const signer = provider.getSigner();
-      const dashboardContractAddress = process.env.REACT_APP_DASHBOARD_CONTRACT_ADDRESS; // Use environment variable
+      const dashboardContractAddress = '0x123...'; // Replace with actual contract address
       const dashboardContractABI = [{...}]; // Replace with actual contract ABI
       const dashboardContract = new ethers.Contract(dashboardContractAddress, dashboardContractABI, signer);
       setDashboardContract(dashboardContract);
@@ -30,12 +27,8 @@ function DashboardComponent(props) {
   useEffect(() => {
     async function fetchUserProfile() {
       if (dashboardContract) {
-        const result = await dashboardContract.viewProfile();
-        setUserAddress(result[0]);
-        setUserName(result[1]);
-        setUserEmail(result[2]);
-        setUserBio(result[3]);
-        setProfileImageUrl(result[4]);
+        const userProfile = await dashboardContract.getUserProfile();
+        setUserProfile(userProfile);
       }
     }
     fetchUserProfile();
@@ -43,11 +36,11 @@ function DashboardComponent(props) {
 
   return (
     <div>
-      <h1>Welcome to your Dashboard, {userName}!</h1>
-      <p>Address: {userAddress}</p>
-      <p>Email: {userEmail}</p>
-      <p>Bio: {userBio}</p>
-      <img src={profileImageUrl} alt="Profile" />
+      <h1>Welcome to your Dashboard, {userProfile.name}!</h1>
+      <p>Address: {userProfile.address}</p>
+      <p>Email: {userProfile.email}</p>
+      <p>Bio: {userProfile.bio}</p>
+      <img src={userProfile.profileImageUrl} alt="Profile" />
     </div>
   );
 }
