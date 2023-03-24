facc05ba0c3c2085db2b6b88c75e04e0bee14624
Devilking6105 <43361618+MichaelHDesigns@users.noreply.github.com>
2023-03-24 06:28:30 -0400

    Update CreateNFT.sol

--- contracts/CreateNFT.sol
+++ contracts/CreateNFT.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "../node_modules/@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "../node_modules/@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
 import "./Create_Account.sol";
 
 contract CreateNFT is ERC721 {
