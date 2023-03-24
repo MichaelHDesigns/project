1796b60dc0b2b034e8b0ef4447346641d7b3834a
Devilking6105 <43361618+MichaelHDesigns@users.noreply.github.com>
2023-03-24 04:51:58 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
-
 import React, { useState } from 'react';
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
 import Create_Account from './components/Create_Account';
 import Dashboard from './components/Dashboard';
 import EditProfile from './components/ProfileEditor';
@@ -14,34 +13,10 @@ import './css/index.css';
 
 function App() {
   const [loggedIn, setLoggedIn] = useState(false);
-  const [searchTerm, setSearchTerm] = useState("");
-
-  const handleSearchChange = (event) => {
-    setSearchTerm(event.target.value);
-  }
 
   return (
     <Router>
       <div className="App">
-        <nav className="navbar">
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/marketplace">Marketplace</Link>
-            </li>
-            <li>
-              <Link to="/create_nft">Create NFT</Link>
-            </li>
-            <li>
-              <form>
-                <input type="text" placeholder="Search" value={searchTerm} onChange={handleSearchChange} />
-                <button type="submit">Search</button>
-              </form>
-            </li>
-          </ul>
-        </nav>
         <Switch>
           <Route exact path="/" component={LandingPage} />
           <Route exact path="/login" render={() => <Login setLoggedIn={setLoggedIn} />} />
@@ -52,7 +27,9 @@ function App() {
               <Route exact path="/profile/edit" component={EditProfile} />
             </Profile>
           </Route>
-          <Route exact path="/create_nft" component={CreateNFTPage} />
+          <Route exact path="/create_nft" render={() => (
+            loggedIn ? <CreateNFTPage /> : <Redirect to="/login" />
+          )} />
           <Route exact path="/marketplace" component={Marketplace} />
         </Switch>
       </div>
