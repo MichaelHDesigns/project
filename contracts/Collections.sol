8bf44fbf179639cfe90dc77cdda36e64a7fb5fa5
Devilking6105 <43361618+MichaelHDesigns@users.noreply.github.com>
2023-03-24 13:05:42 -0400

    Update Collections.sol

--- contracts/Collections.sol
+++ contracts/Collections.sol
@@ -1,97 +1,74 @@
-// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-// Import necessary contracts from OpenZeppelin
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "hardhat/console.sol";
+import "https://api.pinata.cloud/pinning/pinJSONToIPFS";
 
-import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
-import "@openzeppelin/contracts/utils/Strings.sol";                                    
-
-contract Collections {
-    address private nftAddress;
-    string private pinataApiKey;
-    string private pinataSecretApiKey;
-    string private apiUrl = "https://api.pinata.cloud/pinning/pinFileToIPFS";
-
-    constructor(address _nftAddress, string memory _pinataApiKey, string memory _pinataSecretApiKey) {
-        nftAddress = _nftAddress;
-        pinataApiKey = _pinataApiKey;
-        pinataSecretApiKey = _pinataSecretApiKey;
+contract Collections is ERC721, Ownable {
+    struct Collection {
+        string name;
+        string symbol;
+        string[] metadataFields;
     }
 
-    function createCollection(string memory collectionName, string memory collectionDescription, string memory collectionImage) external returns (uint256) {
-        string memory tokenURI = _createTokenURI(collectionName, collectionDescription, collectionImage);
-        (bool success, bytes memory data) = nftAddress.staticcall(
-            abi.encodeWithSignature("createNFT(string,address)", tokenURI, msg.sender)
-        );
-        require(success, "Failed to create NFT");
-        return abi.decode(data, (uint256));
+    Collection[] public collections;
+
+    mapping(uint256 => string[]) private tokenMetadata;
+
+    constructor() ERC721("Collections", "COLL") {}
+
+    function createCollection(string memory _name, string memory _symbol, string[] memory _metadataFields) public onlyOwner returns (uint256) {
+        uint256 newCollectionId = collections.length;
+        Collection memory newCollection = Collection({ name: _name, symbol: _symbol, metadataFields: _metadataFields });
+        collections.push(newCollection);
+
+        _safeMint(msg.sender, newCollectionId);
+        return newCollectionId;
     }
 
-    function _createTokenURI(string memory collectionName, string memory collectionDescription, string memory collectionImage) private returns (string memory) {
-        string[4] memory parts;
-        parts[0] = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 350 350"><rect width="100%" height="100%" fill="#FFF"/> <text x="10" y="20" font-size="20" font-weight="bold" font-family="sans-serif">Collection Name: ';
-        parts[1] = collectionName;
-        parts[2] = '</text><text x="10" y="50" font-size="20" font-weight="bold" font-family="sans-serif">Description: ';
-        parts[3] = collectionDescription;
-        string memory output = string(abi.encodePacked(parts[0], parts[1], parts[2], parts[3]));
-        if (bytes(collectionImage).length > 0) {
-            string memory imageURI = _uploadToPinata(collectionImage);
-            output = string(abi.encodePacked(output, '<image x="10" y="80" width="300" height="200" href="', imageURI, '"/></svg>'));
-        } else {
-            output = string(abi.encodePacked(output, '</svg>'));
+    function addTokenMetadata(uint256 _tokenId, string[] memory _metadata) public {
+        require(_isApprovedOrOwner(msg.sender, _tokenId), "Collections: caller is not owner nor approved");
+
+        Collection storage collection = collections[_tokenId];
+        require(_metadata.length == collection.metadataFields.length, "Collections: incorrect number of metadata fields");
+
+        string memory metadataUri = createMetadataUri(_metadata);
+        tokenMetadata[_tokenId] = _metadata;
+
+        emit URI(metadataUri, _tokenId);
+    }
+
+    function tokenMetadataByIndex(uint256 _tokenId, uint256 _index) public view returns (string memory) {
+        require(_index < collections[_tokenId].metadataFields.length, "Collections: index out of range");
+
+        return tokenMetadata[_tokenId][_index];
+    }
+
+    function createMetadataUri(string[] memory _metadata) private returns (string memory) {
+        string[] memory parts = new string[](_metadata.length * 2 + 1);
+        parts[0] = "data:application/json;base64,";
+        for (uint256 i = 0; i < _metadata.length; i++) {
+            parts[i * 2 + 1] = collections[msg.tokenId].metadataFields[i];
+            parts[i * 2 + 2] = _metadata[i];
         }
-        return output;
+        string memory json = string(abi.encodePacked("{", string(abi.encodePacked(parts)), "}")));
+
+        return pinJsonToIPFS(json);
     }
 
-    function _uploadToPinata(string memory collectionImage) private returns (string memory) {
-        bytes memory imageBytes = bytes(collectionImage);
-        string memory fileName = string(abi.encodePacked("collectionImage_", block.timestamp, ".png"));
-        (uint256 statusCode, bytes memory result) = _pinFileToIPFS(imageBytes, fileName);
-        require(statusCode == 200, "Error uploading image to Pinata");
-        string memory ipfsHash = _extractIpfsHash(result);
-        string memory imageURI = string(abi.encodePacked("https://gateway.pinata.cloud/ipfs/", ipfsHash));
-        return imageURI;
+    function pinJsonToIPFS(string memory _json) private returns (string memory) {
+        bytes memory jsonBytes = bytes(_json);
+        bytes memory resultHash = new bytes(46);
+
+        // Pin JSON to IPFS using Pinata
+        // To use Pinata, you will need to create an account on their website (https://pinata.cloud/)
+        // and obtain an API key and API secret, which you can then use in the code below.
+        // Note: Be sure to keep your API secret secure, and do not hardcode it into your code!
+        string memory pinataApiKey = "<your_pinata_api_key>";
+        string memory pinataApiSecret = "<your_pinata_api_secret>";
+        pinJSONToIPFS(pinataApiKey, pinataApiSecret, jsonBytes, resultHash);
+
+        return string(resultHash);
     }
-
-    function _pinFileToIPFS(bytes memory file, string memory fileName) private returns (uint256, bytes memory) {
-        string memory boundary = "----UploadBoundary----";
-        string memory contentType = "multipart/form-data; boundary=";
-
-        bytes memory requestBody = abi.encodePacked(
-            "--", boundary, "\
-        bytes memory requestBody = abi.encodePacked(
-            "--", boundary, "\r\n",
-            'Content-Disposition: form-data; name="file"; filename="', fileName, '"\"\r\n",
-            "Content-Type: application/octet-stream\r\n\r\n", file, "\r\n--", boundary, "--\r\n");
-
-    (bool success, bytes memory data) = address(nft).staticcall(
-        abi.encodeWithSignature(
-            "post(string,string,bytes,string)",
-            apiUrl,
-            contentType,
-            requestBody,
-            pinataApiKey
-        )
-    );
-
-    require(success, "Failed to post file to Pinata");
-    return abi.decode(data, (uint256, bytes));
-}
-
-function _extractIpfsHash(bytes memory data) private pure returns (string memory) {
-    string memory result;
-    uint256 pointer = 0;
-    while (pointer < data.length) {
-        if (data[pointer] == 0x22 && data[pointer + 1] == 0x49 && data[pointer + 2] == 0x70 && data[pointer + 3] == 0x66 && data[pointer + 4] == 0x73 && data[pointer + 5] == 0x48 && data[pointer + 6] == 0x61 && data[pointer + 7] == 0x73 && data[pointer + 8] == 0x68 && data[pointer + 9] == 0x22 && data[pointer + 10] == 0x3A && data[pointer + 11] == 0x22) {
-            pointer += 12;
-            while (data[pointer] != 0x22) {
-                result = string(abi.encodePacked(result, string(bytes1(data[pointer]))));
-                pointer++;
-            }
-            break;
-        }
-        pointer++;
-    }
-    return result;
-}
 }
