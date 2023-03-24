5366f1a488bb63427ecfca103e7261929313f25b
Devilking6105 <43361618+MichaelHDesigns@users.noreply.github.com>
2023-03-24 03:01:55 -0400

    Update deploy.js

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -5,25 +5,20 @@ async function main() {
 
   console.log("Deploying contracts with the account:", deployer.address);
 
- // const accountAddress = '0x114A612929c451417E28F0Bf9Af3C77c39fd1499'; // Initialize account with an address
-  
   const tokenFactory = await hre.ethers.getContractFactory("AuthToken");
   const token = await tokenFactory.deploy();
   
   const accountFactory = await hre.ethers.getContractFactory("Create_Account");
   const account = await accountFactory.deploy(account.address);
-  const login = await accountFactory.deploy(account.address);
-
-
 
   const profileFactory = await hre.ethers.getContractFactory("Profile");
   const profile = await profileFactory.deploy(account.address);
 
-  const nftFactory = await hre.ethers.getContractFactory("CreateNFT");
-  const nft = await nftFactory.deploy(100, ethers.utils.parseEther("1"));
-
   const dashboardFactory = await hre.ethers.getContractFactory("Dashboard");
   const dashboard = await dashboardFactory.deploy(account.address);
+
+  const nftFactory = await hre.ethers.getContractFactory("CreateNFT");
+  const nft = await nftFactory.deploy(100, ethers.utils.parseEther("1"));
 
   const marketplaceFactory = await hre.ethers.getContractFactory("Marketplace");
   const marketplace = await marketplaceFactory.deploy(account.address, token.address);
@@ -31,21 +26,26 @@ async function main() {
   const editprofileFactory = await hre.ethers.getContractFactory("ProfileEditor");
   const editprofile = await editprofileFactory.deploy(account.address, token.address);
 
+  const loginFactory = await hre.ethers.getContractFactory("Login");
+  const login = await loginFactory.deploy(account.address, token.address);
+
   await token.deployed();
   await account.deployed();
   await profile.deployed();
-  await nft.deployed();
   await dashboard.deployed();
+  await nft.deployed();
   await marketplace.deployed();
+  await editprofile.deployed();
   await login.deployed();
 
   console.log("Token deployed to:", token.address);
   console.log("CreateAccount deployed to:", account.address);
-  console.log("CreateNFT deployed to:", nft.address);
-  console.log("Dashboard deployed to:", dashboard.address);
-  console.log("Login deployed to:", login.address);
   console.log("Profile deployed to:", profile.address);
+  console.log("Dashboard deployed to:", dashboard.address);
+  console.log("CreateNFT deployed to:", nft.address);
   console.log("Marketplace deployed to:", marketplace.address);
+  console.log("ProfileEditor deployed to:", editprofile.address);
+  console.log("Login deployed to:", login.address);
 }
 
 main().then(() => process.exit(0)).catch(error => {
