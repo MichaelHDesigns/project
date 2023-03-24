ad5954676e5aca70d5706e14c124b259225a8613
Devilking6105 <43361618+MichaelHDesigns@users.noreply.github.com>
2023-03-24 16:39:19 -0400

    Update Collections.sol

--- contracts/Collections.sol
+++ contracts/Collections.sol
@@ -1,9 +1,10 @@
+// SPDX-License-Identifier: MIT
+
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "hardhat/console.sol";
-import "https://api.pinata.cloud/pinning/pinFileToIPFS";
 
 contract Collections is ERC721, Ownable {
     struct Collection {
@@ -15,6 +16,7 @@ contract Collections is ERC721, Ownable {
     Collection[] public collections;
 
     mapping(uint256 => string[]) private tokenMetadata;
+    mapping(uint256 => address) private collectionContracts;
 
     constructor() ERC721("Collections", "COLL") {}
 
@@ -30,49 +32,26 @@ contract Collections is ERC721, Ownable {
     function addTokenMetadata(uint256 _tokenId, string[] memory _metadata) public {
         require(_isApprovedOrOwner(msg.sender, _tokenId), "Collections: caller is not owner nor approved");
 
-        Collection storage collection = collections[_tokenId];
-        require(_metadata.length == collection.metadataFields.length, "Collections: incorrect number of metadata fields");
+        address collectionContract = collectionContracts[_tokenId];
+        require(collectionContract != address(0), "Collections: invalid collection contract");
 
-        string memory metadataUri = createMetadataUri(_metadata);
-        tokenMetadata[_tokenId] = _metadata;
+        CollectionData(collectionContract).addTokenMetadata(_tokenId, _metadata);
+    }
 
-        emit URI(metadataUri, _tokenId);
+    function setCollectionContract(uint256 _tokenId, address _collectionContract) public onlyOwner {
+        require(_collectionContract != address(0), "Collections: invalid collection contract");
+        collectionContracts[_tokenId] = _collectionContract;
     }
 
     function tokenMetadataByIndex(uint256 _tokenId, uint256 _index) public view returns (string memory) {
-        require(_index < collections[_tokenId].metadataFields.length, "Collections: index out of range");
+        address collectionContract = collectionContracts[_tokenId];
+        require(collectionContract != address(0), "Collections: invalid collection contract");
 
-        return tokenMetadata[_tokenId][_index];
+        return CollectionData(collectionContract).tokenMetadataByIndex(_tokenId, _index);
     }
-
-function createMetadataUri(string[] memory _metadata) private returns (string memory) {
-    string[] memory parts = new string[](_metadata.length * 2 + 1);
-    parts[0] = "data:application/json;base64,";
-
-    for (uint256 i = 0; i < _metadata.length; i++) {
-        parts[i * 2 + 1] = collections[msg.tokenId].metadataFields[i];
-        parts[i * 2 + 2] = _metadata[i];
-    }
-
-    string memory json = string(abi.encodePacked("{", string(abi.encodePacked(parts)), "}"));
-
-    // Pin JSON to IPFS using Pinata
-    string memory pinataApiKey = "<your_pinata_api_key>";
-    string memory pinataApiSecret = "<your_pinata_api_secret>";
-
-    // Encode the JSON as bytes
-    bytes memory jsonBytes = bytes(json);
-
-    // Set the pinata options
-    string memory pinataOptions = '{"pinataMetadata": {"name": "Collections metadata"}}';
-
-    // Make the API request
-    (bool success, bytes memory data) = address(0x8BCCBEF7f1E1Df277859A2Db030f45E7aB9e45Df).call(abi.encodeWithSignature("pinFileToIPFS(string,string,bytes,string)", pinataApiKey, pinataApiSecret, jsonBytes, pinataOptions));
-    require(success, "Failed to pin JSON to IPFS");
-
-    // Decode the response
-    string memory result = abi.decode(data, (string));
-
-    return result;
 }
+
+abstract contract CollectionData {
+    function addTokenMetadata(uint256 _tokenId, string[] memory _metadata) public virtual;
+    function tokenMetadataByIndex(uint256 _tokenId, uint256 _index) public view virtual returns (string memory);
 }
