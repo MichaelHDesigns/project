0dc68ac2af0582ecea282dfa15b16e6fc7af557b
Devilking6105 <43361618+MichaelHDesigns@users.noreply.github.com>
2023-03-24 06:41:40 -0400

    Update CreateNFT.sol

--- contracts/CreateNFT.sol
+++ contracts/CreateNFT.sol
@@ -2,10 +2,10 @@
 
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
-contract CreateNFT is ERC721 {
+contract CreateNFT is ERC721URIStorage {
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
     address private _owner;
