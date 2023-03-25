00106f051e7acf5514fbfa8c98d0b1486f5a9ff1
Devilking6105 <43361618+MichaelHDesigns@users.noreply.github.com>
2023-03-25 01:41:20 -0400

    Update Login.jsx

--- js/Login.jsx
+++ js/Login.jsx
@@ -1,13 +1,13 @@
 import React, { useState } from 'react';
 import { Redirect } from 'react-router-dom';
-import { getWeb3, getSingleMint } from '../utils/getWeb3';
 import AuthToken from '../contracts/AuthToken.sol';
+import { getWeb3, getSingleMint } from '../utils.js';
 import './Login.css';
 
 const Login = ({ setLoggedIn }) => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [error, setError] = useState("");
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
 
   const handleLogin = async (e) => {
     e.preventDefault();
@@ -20,32 +20,32 @@ const Login = ({ setLoggedIn }) => {
       if (loginResult.status) {
         setLoggedIn(true);
       } else {
-        setError("Invalid email or password");
+        setError('Invalid email or password');
       }
     } catch (error) {
-      setError("Error while logging in");
+      setError('Error while logging in');
       console.log(error);
     }
   };
 
-  if (localStorage.getItem("authToken")) {
-    return <Redirect to="/dashboard" />;
+  if (localStorage.getItem('authToken')) {
+    return <Redirect to='/dashboard' />;
   }
 
   return (
-    <div className="login-container">
+    <div className='login-container'>
       <h2>Login</h2>
       <form onSubmit={handleLogin}>
-        <label htmlFor="email">Email:</label>
-        <input type="email" id="email" value={email} onChange={(e) => setEmail(e.target.value)} required />
+        <label htmlFor='email'>Email:</label>
+        <input type='email' id='email' value={email} onChange={(e) => setEmail(e.target.value)} required />
 
-        <label htmlFor="password">Password:</label>
-        <input type="password" id="password" value={password} onChange={(e) => setPassword(e.target.value)} required />
+        <label htmlFor='password'>Password:</label>
+        <input type='password' id='password' value={password} onChange={(e) => setPassword(e.target.value)} required />
 
-        <button type="submit">Login</button>
+        <button type='submit'>Login</button>
       </form>
 
-      {error && <p className="error">{error}</p>}
+      {error && <p className='error'>{error}</p>}
     </div>
   );
 };
